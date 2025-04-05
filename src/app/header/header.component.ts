6606cb77bf5b375e2e0b9a8350a6d77e11418a8a
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-05 20:43:49 +0330

    feat: add navigation icons to the project

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  lucideHouse,
+  lucidePalette,
+  lucideMail,
+  lucideBookMarked,
+} from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
+  viewProviders: [
+    provideIcons({ lucideHouse, lucidePalette, lucideMail, lucideBookMarked }),
+  ], // Add the icon to the provider
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   isMobileMenuActive = signal<boolean>(false);
+
   onMobileMenuBtnClicked() {
     this.isMobileMenuActive.set(!this.isMobileMenuActive());
   }

17bc67e1f82241c0748dc839dd7e6fd8d17a91b4
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-05 17:14:24 +0330

    feat: add click even for mobile menu button

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  isMobileMenuActive = signal<boolean>(false);
+  onMobileMenuBtnClicked() {
+    this.isMobileMenuActive.set(!this.isMobileMenuActive());
+  }
 }
